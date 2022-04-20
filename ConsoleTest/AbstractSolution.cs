e1dd612bbc3a79dc845b55bbdfd1e17ce97820f9
QuyVu <quy.vu.0101@gmail.com>
2022-04-20 17:11:51 +0700

    Initial commit

---
+++ ConsoleTest/AbstractSolution.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using OneOf;
+
+namespace ConsoleTest
+{
+    public enum Error
+    {
+        DrinkingOutOfRange,
+        NumbersToGetGiftError,
+        PresentOverCondition
+    }
+
+    public static class AbstractSolution
+    {
+        /// <summary>
+        /// Drinking plan: Total number of Drinking plan
+        /// NumbersToGetGift: Number of Per drinking plan to get the gift
+        /// NumberGiftPerOne: Number of gift per one times, when we got enough Drinking
+        /// </summary>
+        /// <param name="drinkingPlan"></param>
+        /// <param name="numbersToGetGift"></param>
+        /// <param name="numberGiftPerOne"></param>
+        /// <returns></returns>
+        public static OneOf<int, Error> CalculateTheQuantity(int drinkingPlan, int numbersToGetGift,
+            int numberGiftPerOne)
+        {
+            if (drinkingPlan < 0 || drinkingPlan > Math.Pow(10, 5)) return Error.DrinkingOutOfRange;
+            if (numbersToGetGift < 1) return Error.NumbersToGetGiftError;
+            if (numberGiftPerOne >= numbersToGetGift) return Error.PresentOverCondition;
+            if (drinkingPlan < numbersToGetGift) return drinkingPlan;
+            var numOfGetGift = ((drinkingPlan - numbersToGetGift) % (numbersToGetGift - numberGiftPerOne)) switch
+            {
+                0 => (drinkingPlan - numbersToGetGift) / (numbersToGetGift - numberGiftPerOne) + 1,
+                _ => (int) Math.Ceiling((float) (drinkingPlan - numbersToGetGift) /
+                                        (numbersToGetGift - numberGiftPerOne))
+            };
+            return drinkingPlan + numOfGetGift * numberGiftPerOne;
+        }
+    }
+}
---
+++ ConsoleTest/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using ConsoleTest;
+
+// For this case: 11 is total drinking plan. 3 is number of condition to get the gift, 1 is the gift
+var res = AbstractSolution.CalculateTheQuantity(11, 3, 1);
+res.Switch(Console.WriteLine, e => Console.WriteLine($"Error is : {e}"));
